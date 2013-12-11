13da13b7f8066e24d31341d38442c5a50f31acb4
nlmartinez <nmartin1@harding.edu>
2013-12-11 17:46:37 -0600

    finished flipping pancakes
    finished!!

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿static void Main()
+{
+    int x = 5;
+    int y = x;
+     
+    string s = "Hello";
+    s = "Goodbye";
+     
+    MyInt a = new MyInt();
+    a.value = 10;
+     
+    MyInt b = a;
+    b.value = y;
+     
+    Console.WriteLine(a.value);
+     
+    Test(a);   
+    Console.WriteLine(a.value);
+     
+    Test2(ref a);  
+    Console.WriteLine(a.value);
+}
+ 
+class MyInt
+{
+    public int value;
+}
+ 
+void Test(MyInt num)
+{
+    num.value = 25;
+    num = null;
+}
+ 
+void Test2(ref MyInt num)
+{
+    num.value = 30;
+    num = null;
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- FlippingPancakes/FlippingPancakes/flipping.cpp
+++ FlippingPancakes/FlippingPancakes/flipping.cpp
@@ -1,7 +1,10 @@
 #include<fstream>
 #include<string>
 #include<queue>
+#include<set>
 #include<iostream>
+#include<algorithm>
+#include<cctype>
 
 using std::string;
 using std::queue;
@@ -10,21 +13,64 @@ using std::ifstream;
 using std::ofstream;
 using std::cout;
 using std::getline;
+using std::set;
 
 struct Stack
 {
 	string config;
-	int position;
+	int flip;
 };
-
-Stack FlipStack (Stack pancakes)
-{
-
-}
+queue<Stack> currentOptions;
+set<string> usedCombinations;
+set<string>::iterator it;
 
 string orderPancakes(string config)
 {
+	int stackSize = config.length();
 
+	switch (stackSize)
+	{
+	case 1:
+		return config;
+	case 2: 
+		if(config.at(0) < config.at(1)){
+			return config;
+		}
+		else
+		{
+			string temp = config.substr(1);
+			temp += config.substr(0,1);
+			return temp;
+		}
+	default:		
+			string idealConfig;
+			
+			while(config.length() > 0)
+			{
+				string temp = config.substr(0,1);
+				int position = 0;
+				for(int i = 0; i < config.length(); i ++)
+				{
+					if(temp > config.substr(i,1))
+					{
+						temp = config.substr(i,1);
+						position = i;
+					}
+				}
+				
+				idealConfig += temp;
+				if(position == 0)
+					config = config.substr(1);
+				else if(position == config.length() - 1)
+					config = config.substr(0, config.length() - 1);					
+				else
+				{
+					config = config.substr(0, position) + config.substr(position + 1);
+				}
+			}
+
+			return idealConfig;
+	}
 }
 
 string findIdealConfig(string initialConfig)
@@ -37,16 +83,46 @@ string findIdealConfig(string initialConfig)
 	return result;
 }
 
-string flipThree(string config)
+//Make a flip utility function
+string ChangeCase(string temp)
 {
+	for(int i = 0; i < temp.length(); i++)
+	{
+		if(std::isupper(temp.at(i)))
+			temp.at(i) = std::tolower(temp.at(i));
+		else
+			temp.at(i) = std::toupper(temp.at(i));
+	}
 
+	return temp;
 }
 
-string flipTwo(string config)
-{}
+void Flip(Stack initialConfig)
+{
+	int currentFlip = initialConfig.flip + 1;
+	Stack pancakes;
 
-string flipOne(string config)
-{}
+	for(int i = 0; i < initialConfig.config.length(); i++)
+	{
+		string temp = initialConfig.config.substr(i);
+		string newConfig = initialConfig.config.substr(0,i);
+
+		temp = string (temp.rbegin(), temp.rend());
+		temp = ChangeCase(temp);
+
+		newConfig.append(temp);
+
+		if(usedCombinations.find(newConfig) == usedCombinations.end())
+		{
+			usedCombinations.insert(newConfig);
+			pancakes.config = newConfig;
+			pancakes.flip = currentFlip;
+			currentOptions.push(pancakes);
+		}
+	}
+}
+
+
 
 int main()
 {
@@ -55,15 +131,42 @@ int main()
 	if(inFile)
 	{
 		ofstream outFile("pancakes.out");
-		string initialConfig;
-		getline(inFile, initialConfig);
-		while(initialConfig == "0")
+		Stack pancakes;
+		getline(inFile, pancakes.config);
+		pancakes.flip = 0;
+		while(pancakes.config != "0")
 		{
+			usedCombinations.insert(pancakes.config);
+			currentOptions.push(pancakes);
+			string idealConfig = findIdealConfig(pancakes.config);
+			
+			while(!currentOptions.empty())
+			{
+				Stack nextStack = currentOptions.front(); currentOptions.pop();
 
+				if(nextStack.config.compare(idealConfig) == 0)
+				{
+					outFile << nextStack.flip << endl;
+					while(!currentOptions.empty())
+						currentOptions.pop();
+					usedCombinations.clear();
+				}
+				else
+				{
+					Flip(nextStack);
+				}
+
+			}
+			getline(inFile, pancakes.config);
+			pancakes.flip = 0;
 		}
+		
+		outFile.close();
 	}
 	else
 	{
 		cout << "Could not find pancakes.in file." << endl;
 	}
+	
+	inFile.close();
 }
